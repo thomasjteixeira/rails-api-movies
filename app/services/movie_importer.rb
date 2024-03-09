5c5d068b2d4908eda9b3e8577db3e93950d448e2
Thómas Jéfferson <thomasjteixeira@gmail.com>
2024-03-09 12:23:33 -0300

    :test_tube: test: add movie_importer services

--- app/services/movie_importer.rb
+++ app/services/movie_importer.rb
@@ -1,28 +1,32 @@
 class MovieImporter
-  def self.import_movie_tmdb(id: nil, title: nil)
-    tmdb_movie = fetch_tmdb_movie(id:, title:)
-    create_movie_from_tmdb_movie(tmdb_movie)
-  end
-
-  def self.fetch_tmdb_movie(id: nil, title: nil)
-    if id
-      Tmdb::Movie.detail(id)
-    else
-      search_results = Tmdb::Movie.find(title).results
-      search_results.present ? Tmdb::Movie.detail(search_results.first.id) : nil
+  class << self
+    def import_movie_tmdb(id: nil, title: nil)
+      tmdb_movie = fetch_tmdb_movie(id:, title:)
+      create_movie_from_tmdb_movie(tmdb_movie)
     end
-  end
 
-  def self.create_movie_from_tmdb_movie(tmdb_movie)
-    return unless tmdb_movie
+    private
 
-    movie_attributes = extract_movie_attributes(tmdb_movie)
-    Movie.create!(movie_attributes)
-  end
+    def fetch_tmdb_movie(id: nil, title: nil)
+      return Tmdb::Movie.detail(id) if id
 
-  def self.extract_movie_attributes(tmdb_movie)
-    Movie.attribute_names.each_with_object({}) do |attr, hash|
-      hash[attr] = tmdb_movie.send(attr) if tmdb_movie.respond_to?(attr)
+      search_results = Tmdb::Search.movie(title).results
+      return Tmdb::Movie.detail(search_results.first.id) if search_results.present?
+
+      raise 'Movie not found on TMDb.'
+    end
+
+    def create_movie_from_tmdb_movie(tmdb_movie)
+      return unless tmdb_movie
+
+      movie_attributes = extract_movie_attributes(tmdb_movie)
+      Movie.create!(movie_attributes)
+    end
+
+    def extract_movie_attributes(tmdb_movie)
+      Movie.attribute_names.each_with_object({}) do |attr, hash|
+        hash[attr] = tmdb_movie.send(attr) if tmdb_movie.respond_to?(attr)
+      end
     end
   end
 end
